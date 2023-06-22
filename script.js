18b7693e7f0fdb081e6598d600d83d6b2b856158
Claquettes <math@claq.fr>
2023-06-22 16:51:08 +0200

    three types of particules + grey type that has no gravity and stop others

--- script.js
+++ script.js
@@ -1,39 +1,101 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const canvas = document.getElementById('canvas');
-    const ctx = canvas.getContext('2d');
-    // Define the size of the canvas, 1/2 of the window width and full height
-    const size = window.innerWidth / 4;
-    canvas.width = size;
-    canvas.height = size;
-    
-    // Initialize the canvas with black tiles
-    ctx.fillStyle = 'black';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    
-    // Add click event listener to the canvas
-    canvas.addEventListener('click', function(e) {
-      const rect = canvas.getBoundingClientRect();
-      const x = e.clientX - rect.left;
-      const y = e.clientY - rect.top;
-      
-      // Calculate the size of each tile
-      const tileSize = canvas.width / 400;
-      
-      // Calculate the tile index
-      const rowIndex = Math.floor(y / tileSize);
-      const columnIndex = Math.floor(x / tileSize);
-      
-      // Calculate the tile position
-      const tileX = columnIndex * tileSize;
-      const tileY = rowIndex * tileSize;
-      
-      // Change the color of the clicked tile and neighboring tiles to white
-      ctx.fillStyle = 'white';
-      const sizeIncrease = 3; // Increase the tile size by 3 pixels
-      const expandedTileX = tileX - sizeIncrease;
-      const expandedTileY = tileY - sizeIncrease;
-      const expandedTileSize = tileSize + sizeIncrease * 2;
-      ctx.fillRect(expandedTileX, expandedTileY, expandedTileSize, expandedTileSize);
-    });
-  });
-  
+// Constants
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+const gridSize = 7;
+const gridWidth = 700;
+const gridHeight = 700;
+const numCols = gridWidth / gridSize;
+const numRows = gridHeight / gridSize;
+const colors = ['red', 'green', 'blue', 'grey']; // Added 'grey' color
+
+// Initialize grid
+const grid = [];
+for (let i = 0; i < numRows; i++) {
+  const row = [];
+  for (let j = 0; j < numCols; j++) {
+    row.push(''); // Changed to an empty tile
+  }
+  grid.push(row);
+}
+
+// Mouse variables
+let isMouseDown = false;
+let currentColor = 'red';
+
+// Event listeners
+canvas.addEventListener('mousedown', handleMouseDown);
+canvas.addEventListener('mouseup', handleMouseUp);
+canvas.addEventListener('mousemove', handleMouseMove);
+
+const redButton = document.getElementById('redButton');
+const greenButton = document.getElementById('greenButton');
+const blueButton = document.getElementById('blueButton');
+const greyButton = document.getElementById('greyButton');
+
+redButton.addEventListener('click', () => setCurrentColor('red'));
+greenButton.addEventListener('click', () => setCurrentColor('green'));
+blueButton.addEventListener('click', () => setCurrentColor('blue'));
+greyButton.addEventListener('click', () => setCurrentColor('grey'));
+
+// Functions
+function drawGrid() {
+  ctx.clearRect(0, 0, gridWidth, gridHeight);
+  for (let i = 0; i < numRows; i++) {
+    for (let j = 0; j < numCols; j++) {
+      const color = grid[i][j];
+      if (color !== '') {
+        ctx.fillStyle = color;
+        ctx.fillRect(j * gridSize, i * gridSize, gridSize, gridSize);
+      }
+    }
+  }
+}
+
+function handleMouseDown(event) {
+  isMouseDown = true;
+}
+
+function handleMouseUp(event) {
+  isMouseDown = false;
+}
+
+function handleMouseMove(event) {
+  if (isMouseDown) {
+    const rect = canvas.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+    const col = Math.floor(x / gridSize);
+    const row = Math.floor(y / gridSize);
+    if (col >= 0 && col < numCols && row >= 0 && row < numRows) {
+      grid[row][col] = currentColor;
+    }
+  }
+}
+
+function applyGravity() {
+  for (let i = numRows - 2; i >= 0; i--) {
+    for (let j = 0; j < numCols; j++) {
+      if (grid[i][j] !== '' && grid[i + 1][j] === '' && grid[i][j] !== 'grey') {
+        grid[i + 1][j] = grid[i][j];
+        grid[i][j] = '';
+      }
+    }
+  }
+}
+
+const greyColorIndex = 3; // Index of 'grey' in the colors array
+
+function setCurrentColor(color) {
+  if (color === 'grey') {
+    currentColor = colors[greyColorIndex];
+  } else {
+    currentColor = color;
+  }
+}
+
+function update() {
+  applyGravity();
+  drawGrid();
+}
+
+setInterval(update, 100); // Update every 1/10 of a second
