f4354a0b3cfa146e7e5e63d72f383ef8f09e6f5a
Claquettes <math@claq.fr>
2023-06-25 21:21:04 +0200

    buttons in a differents files

--- script.js
+++ script.js
@@ -27,30 +27,6 @@ canvas.addEventListener('mousedown', handleMouseDown);
 canvas.addEventListener('mouseup', handleMouseUp);
 canvas.addEventListener('mousemove', handleMouseMove);
 
-const redButton = document.getElementById('redButton');
-const greenButton = document.getElementById('greenButton');
-const waterButton = document.getElementById('waterButton');
-const greyButton = document.getElementById('greyButton');
-const sandButton = document.getElementById('sandButton'); 
-const coalButton = document.getElementById('coalButton'); 
-const lavaButton = document.getElementById('lavaButton');
-const stoneButton = document.getElementById('stoneButton');
-const freezePowderButton = document.getElementById('freezePowderButton');
-const iceButton = document.getElementById('iceButton');
-const steamButton = document.getElementById('steamButton');
-
-redButton.addEventListener('click', () => setCurrentColor('red'));
-greenButton.addEventListener('click', () => setCurrentColor('green'));
-waterButton.addEventListener('click', () => setCurrentColor('water'));
-greyButton.addEventListener('click', () => setCurrentColor('grey'));
-sandButton.addEventListener('click', () => setCurrentColor('sand'));
-coalButton.addEventListener('click', () => setCurrentColor('coal'));
-lavaButton.addEventListener('click', () => setCurrentColor('lava'));
-stoneButton.addEventListener('click', () => setCurrentColor('stone'));
-freezePowderButton.addEventListener('click', () => setCurrentColor('freezePowder'));
-iceButton.addEventListener('click', () => setCurrentColor('ice'));
-steamButton.addEventListener('click', () => setCurrentColor('steam'));
-
 // Functions
 function drawGrid() {
   ctx.clearRect(0, 0, gridWidth, gridHeight);
