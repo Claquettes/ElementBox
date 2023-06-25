55705360efd333ef68db576c24b3238ff285c668
Claquettes <math@claq.fr>
2023-06-25 21:58:17 +0200

    Thanks @FlashOnFire for tips on positionning and square grid with mouse offset

--- script.js
+++ script.js
@@ -1,72 +1,78 @@
 // Constants
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
-const gridSize = 7;
-const gridWidth = 700;
-const gridHeight = 700;
-const numCols = gridWidth / gridSize;
-const numRows = gridHeight / gridSize;
-const colors = ['red', 'green', 'water', 'grey', 'sand']; // Added 'grey' color
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+const canvasSize = 1000;
+
+const gridSize = canvasSize / 100;
+
+canvas.width = canvasSize;
+canvas.height = canvasSize;
+
+const numCols = Math.ceil(canvasSize / gridSize);
+const numRows = Math.ceil(canvasSize / gridSize);
+const colors = ["red", "green", "water", "grey", "sand"]; // Added 'grey' color
 
 // Initialize grid
 const grid = [];
 for (let i = 0; i < numRows; i++) {
   const row = [];
   for (let j = 0; j < numCols; j++) {
-    row.push(''); // Changed to an empty tile
+    row.push(""); // Changed to an empty tile
   }
   grid.push(row);
 }
 
 // Mouse variables
 let isMouseDown = false;
-let currentColor = 'red';
+let currentColor = "red";
 
 // Event listeners
-canvas.addEventListener('mousedown', handleMouseDown);
-canvas.addEventListener('mouseup', handleMouseUp);
-canvas.addEventListener('mousemove', handleMouseMove);
+canvas.addEventListener("mousedown", handleMouseDown);
+canvas.addEventListener("mouseup", handleMouseUp);
+canvas.addEventListener("mousemove", handleMouseMove);
 
 // Functions
 function drawGrid() {
-  ctx.clearRect(0, 0, gridWidth, gridHeight);
+  ctx.clearRect(0, 0, canvasSize, canvasSize);
   for (let i = 0; i < numRows; i++) {
     for (let j = 0; j < numCols; j++) {
       const color = grid[i][j];
-      if (color !== '') { //if the tile is not empty
+      if (color !== "") {
+        //if the tile is not empty
         switch (color) {
-          case 'red':
+          case "red":
             ctx.fillStyle = redColor;
             break;
-          case 'green':
+          case "green":
             ctx.fillStyle = greenColor;
             break;
-          case 'water':
+          case "water":
             //we put the hex code of the color
             ctx.fillStyle = waterColor;
             break;
-          case 'grey':
+          case "grey":
             ctx.fillStyle = greyColor;
             break;
-          case 'sand':
+          case "sand":
             ctx.fillStyle = sandColor;
             break;
-          case 'coal':
+          case "coal":
             ctx.fillStyle = coalColor;
             break;
-          case 'lava':
+          case "lava":
             ctx.fillStyle = lavaColor;
             break;
-          case 'stone':
+          case "stone":
             ctx.fillStyle = stoneColor;
             break;
-          case 'freezePowder':
+          case "freezePowder":
             ctx.fillStyle = freezePowderColor;
             break;
-          case 'ice':
+          case "ice":
             ctx.fillStyle = iceColor;
             break;
-          case 'steam':
+          case "steam":
             ctx.fillStyle = steamColor;
             break;
         }
@@ -86,14 +92,17 @@ function handleMouseUp(event) {
 
 function handleMouseMove(event) {
   if (isMouseDown) {
-    const rect = canvas.getBoundingClientRect();
-    const x = event.clientX - rect.left;
-    const y = event.clientY - rect.top;
-    const col = Math.floor(x / gridSize);
-    const row = Math.floor(y / gridSize);
+    var rect = canvas.getBoundingClientRect();
+    var widthScale = canvas.width / rect.width;
+    var heightScale = canvas.height / rect.height;
+    var canvasClickX = (event.clientX - rect.left) * widthScale;
+    var canvasClickY = (event.clientY - rect.top) * heightScale;
+
+    const col = Math.floor(canvasClickX / gridSize);
+    const row = Math.floor(canvasClickY / gridSize);
     if (col >= 0 && col < numCols && row >= 0 && row < numRows) {
       //we check if the tile is empty
-      if (grid[row][col] === '') {
+      if (grid[row][col] === "") {
         grid[row][col] = currentColor;
       }
     }
@@ -103,129 +112,125 @@ function handleMouseMove(event) {
 function applyGravity() {
   for (let i = numRows - 2; i >= 0; i--) {
     for (let j = 0; j < numCols; j++) {
-      //we start by checking collision for gases. 
-      if(grid[i][j] === 'steam' && grid[i - 1][j] === ''){
-        if(i - 1 >= 0){
+      //we start by checking collision for gases.
+      if (grid[i][j] === "steam" && grid[i - 1][j] === "") {
+        if (i - 1 >= 0) {
           //10% chance to go up
-          if(Math.random() > 0.667){
-            grid[i - 1][j] = 'steam';
-            grid[i][j] = '';
+          if (Math.random() > 0.667) {
+            grid[i - 1][j] = "steam";
+            grid[i][j] = "";
+          }
+        } else {
+          grid[i][j] = "";
+        }
+      } else {
+        //we check if the tile below is empty and the current tile is not empty and not grey
+        if (
+          grid[i][j] !== "" &&
+          grid[i + 1][j] === "" &&
+          grid[i][j] !== "grey"
+        ) {
+          grid[i + 1][j] = grid[i][j];
+          grid[i][j] = "";
+        }
+        //if a water tile is ON TOP of a water tile
+        if (grid[i][j] === "water" && grid[i + 1][j] === "water") {
+          console.log("water on water");
+          //the tile on the top becomes empty
+          grid[i][j] = "";
+          //we check if the diagonal tiles are empty below
+          if (grid[i + 1][j + 1] === "" && grid[i + 1][j - 1] === "") {
+            //if they are empty, we put a water tile on 50% right or left
+            if (Math.random() > 0.5) {
+              grid[i + 1][j + 1] = "water";
+              console.log("water on water on water right");
+            } else {
+              grid[i + 1][j - 1] = "water";
+              console.log("water on water on water left");
+            }
+          } else if (grid[i + 1][j + 1] === "" && grid[i + 1][j - 1] !== "") {
+            grid[i + 1][j + 1] = "water";
+            console.log("water on water on water right 1");
+          } else if (grid[i + 1][j + 1] !== "" && grid[i + 1][j - 1] === "") {
+            grid[i + 1][j - 1] = "water";
+            console.log("water on water on water left 2");
+          }
+          //if the diagonal tiles are not empty, we check if the tile directly on the right is empty; and the left is empty
+          else if (grid[i][j + 1] === "" && grid[i][j - 1] === "") {
+            //if they are empty, we put a water tile on 50% right or left
+            if (Math.random() > 0.5) {
+              grid[i][j + 1] = "water";
+              console.log("water on water on water right 3");
+            } else {
+              grid[i][j - 1] = "water";
+              console.log("water on water on water left 4");
+            }
+          } else if (grid[i][j + 1] === "" && grid[i][j - 1] !== "") {
+            grid[i][j + 1] = "water";
+            console.log("water on water on water right 5");
+          } else if (grid[i][j + 1] !== "" && grid[i][j - 1] === "") {
+            grid[i][j - 1] = "water";
+            console.log("water on water on water left 6");
+          } else {
+            grid[i][j] = "water";
           }
         }
-        else{
-          grid[i][j] = '';
+        //if a sand tile is ON TOP of a water tile
+        if (grid[i][j] === "sand" && grid[i + 1][j] === "water") {
+          //the tile on the top becomes water
+          grid[i][j] = "water";
+          //the tile below becomes sand
+          grid[i + 1][j] = "sand";
         }
-        
-      }
-      else {
-        
-          //we check if the tile below is empty and the current tile is not empty and not grey
-      if (grid[i][j] !== '' && grid[i + 1][j] === '' && grid[i][j] !== 'grey') {
-        grid[i + 1][j] = grid[i][j];
-        grid[i][j] = '';
-      }
-      //if a water tile is ON TOP of a water tile
-      if(grid[i][j] === 'water' && grid[i + 1][j] === 'water'){
-        console.log('water on water');
-        //the tile on the top becomes empty
-        grid[i][j] = '';
-        //we check if the diagonal tiles are empty below
-        if(grid[i + 1][j + 1] === '' && grid[i + 1][j - 1] === ''){
-            //if they are empty, we put a water tile on 50% right or left
-            if(Math.random() > 0.5){
-                grid[i + 1][j + 1] = 'water';
-                console.log('water on water on water right');
-            }else{
-                grid[i + 1][j - 1] = 'water';
-                console.log('water on water on water left');
-            }
-        }
-        else if(grid[i + 1][j + 1] === '' && grid[i + 1][j - 1] !== ''){
-            grid[i + 1][j + 1] = 'water';
-            console.log('water on water on water right 1');
-        }
-        else if(grid[i + 1][j + 1] !== '' && grid[i + 1][j - 1] === ''){
-            grid[i + 1][j - 1] = 'water';
-            console.log('water on water on water left 2');
-        }
-        //if the diagonal tiles are not empty, we check if the tile directly on the right is empty; and the left is empty
-        else if(grid[i][j + 1] === '' && grid[i][j - 1] === ''){
-            //if they are empty, we put a water tile on 50% right or left
-            if(Math.random() > 0.5){
-                grid[i][j + 1] = 'water';
-                console.log('water on water on water right 3');
-            }else{
-                grid[i][j - 1] = 'water';
-                console.log('water on water on water left 4');
-            }
-        }
-        else if(grid[i][j + 1] === '' && grid[i][j - 1] !== ''){
-            grid[i][j + 1] = 'water';
-            console.log('water on water on water right 5');
-        }
-        else if(grid[i][j + 1] !== '' && grid[i][j - 1] === ''){
-            grid[i][j - 1] = 'water';
-            console.log('water on water on water left 6');
-        }
-        else{
-            grid[i][j] = 'water';
-        }
-      }
-      //if a sand tile is ON TOP of a water tile
-      if(grid[i][j] === 'sand' && grid[i + 1][j] === 'water'){
-        //the tile on the top becomes water
-        grid[i][j] = 'water';
-        //the tile below becomes sand
-        grid[i + 1][j] = 'sand';
-      }
-      //if the tile is lava
-      if(grid[i][j] === 'lava'){
-        //any water tile adjacent to the lava tile becomes stone
-        if(grid[i + 1][j] === 'water'){
-            grid[i + 1][j] = 'stone';
+        //if the tile is lava
+        if (grid[i][j] === "lava") {
+          //any water tile adjacent to the lava tile becomes stone
+          if (grid[i + 1][j] === "water") {
+            grid[i + 1][j] = "stone";
             //we generate steam on top of the lava
-            grid[i-1][j] = 'steam';
+            grid[i - 1][j] = "steam";
+          }
+          if (grid[i - 1][j] === "water") {
+            grid[i - 1][j] = "stone";
+          }
+          if (grid[i][j + 1] === "water") {
+            grid[i][j + 1] = "stone";
+          }
+          if (grid[i][j - 1] === "water") {
+            grid[i][j - 1] = "stone";
+          }
         }
-        if(grid[i - 1][j] === 'water'){
-            grid[i - 1][j] = 'stone';
-        }
-        if(grid[i][j + 1] === 'water'){
-            grid[i][j + 1] = 'stone';
-        }
-        if(grid[i][j - 1] === 'water'){
-            grid[i][j - 1] = 'stone';
+        //if the tile is freezePowder
+        if (grid[i][j] === "freezePowder" || grid[i][j] === "ice") {
+          if (Math.random() > 0.3) {
+            // 50% chance to become ice
+            //any water tile adjacent to the freezePowder or ice tile becomes ice
+            if (grid[i + 1][j] === "water") {
+              grid[i + 1][j] = "ice";
+              grid[i][j] = "ice";
+            }
+            if (grid[i - 1][j] === "water") {
+              grid[i - 1][j] = "ice";
+              grid[i][j] = "ice";
+            }
+            if (grid[i][j + 1] === "water") {
+              grid[i][j + 1] = "ice";
+              grid[i][j] = "ice";
+            }
+            if (grid[i][j - 1] === "water") {
+              grid[i][j - 1] = "ice";
+              grid[i][j] = "ice";
+            }
+          }
         }
       }
-      //if the tile is freezePowder
-      if(grid[i][j] === 'freezePowder' || grid[i][j] === 'ice'){
-        if(Math.random() > 0.3){ // 50% chance to become ice
-        //any water tile adjacent to the freezePowder or ice tile becomes ice
-        if(grid[i + 1][j] === 'water'){
-            grid[i + 1][j] = 'ice';
-            grid[i][j] = 'ice';
-        }
-        if(grid[i - 1][j] === 'water'){
-            grid[i - 1][j] = 'ice';
-            grid[i][j] = 'ice';
-        }
-        if(grid[i][j + 1] === 'water'){
-            grid[i][j + 1] = 'ice';
-            grid[i][j] = 'ice';
-        }
-        if(grid[i][j - 1] === 'water'){
-            grid[i][j - 1] = 'ice';
-            grid[i][j] = 'ice';
-        }
-      }
-    }
     }
   }
-}
 }
 const greyColorIndex = 3; // Index of 'grey' in the colors array
 
 function setCurrentColor(color) {
-  if (color === 'grey') {
+  if (color === "grey") {
     currentColor = colors[greyColorIndex];
   } else {
     currentColor = color;
