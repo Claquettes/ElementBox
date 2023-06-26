00b17118df1d865e470504fbcf35dae673bf7317
Claquettes <math@claq.fr>
2023-06-26 20:19:13 +0200

    particle generator does not dupplicate wires

--- script.js
+++ script.js
@@ -277,16 +277,16 @@ function applyGravity() {
           //we generate a tile below the generator/ the tile generated is of the type as one of the 4 tiles adjacent to the generator
           //we check the 4 tiles adjacent to the generator
           let adjacentTiles = [];
-          if (grid[i + 1][j] !== "" && grid[i + 1][j] !== "generator") {
+          if (grid[i + 1][j] !== "" && grid[i + 1][j] !== "generator" && grid[i + 1][j] !== "wire") {
             adjacentTiles.push(grid[i + 1][j]);
           }
-          if (grid[i - 1][j] !== "" && grid[i - 1][j] !== "generator") {
+          if (grid[i - 1][j] !== "" && grid[i - 1][j] !== "generator" && grid[i - 1][j] !== "wire") {
             adjacentTiles.push(grid[i - 1][j]);
           }
-          if (grid[i][j + 1] !== "" && grid[i][j + 1] !== "generator") {
+          if (grid[i][j + 1] !== "" && grid[i][j + 1] !== "generator" && grid[i][j + 1] !== "wire") {
             adjacentTiles.push(grid[i][j + 1]);
           }
-          if (grid[i][j - 1] !== "" && grid[i][j - 1] !== "generator") {
+          if (grid[i][j - 1] !== "" && grid[i][j - 1] !== "generator" && grid[i][j - 1] !== "wire") {
             adjacentTiles.push(grid[i][j - 1]);
           }
           //we generate a tile below the generator
