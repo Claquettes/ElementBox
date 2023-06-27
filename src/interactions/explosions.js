e978c92144b30c56724e989968c0f266b726a3ea
Claquettes <math@claq.fr>
2023-06-27 14:28:27 +0200

    CHAIN REACTION FOR EXPLOSIVES

--- src/interactions/explosions.js
+++ src/interactions/explosions.js
@@ -1,32 +1,40 @@
-function applyExplosion (i, j, explosiveType){
-    let explosionRadius = 0;    
-    switch(explosiveType){
-        case "tnt":
-            explosionRadius = 4;
-            break;
+function applyExplosion(i, j, explosiveType) {
+  let explosionRadius = 0;
+  switch (explosiveType) {
+    case "tnt":
+      explosionRadius = 4;
+      break;
+  }
+  
+  let tilesInExplosionRadius = [];
+  
+  if (i - explosionRadius < 0) {
+    explosionRadius = i;
+  }
+  if (j - explosionRadius < 0) {
+    explosionRadius = j;
+  }
+  if (i + explosionRadius > grid.length - 1) {
+    explosionRadius = grid.length - 1 - i;
+  }
+  if (j + explosionRadius > grid[0].length - 1) {
+    explosionRadius = grid[0].length - 1 - j;
+  }
+  
+  for (let k = i - explosionRadius; k <= i + explosionRadius; k++) {
+    for (let l = j - explosionRadius; l <= j + explosionRadius; l++) {
+      tilesInExplosionRadius.push([k, l]);
+      
+      if (k === i && l === j) {
+        // Explode the explosive tile itself
+        grid[k][l] = "";
+      } else if (grid[k][l] === explosiveType) {
+        // Trigger chain reaction for explosive tiles within the radius
+        applyExplosion(k, l, explosiveType);
+      } else {
+        // Explode other tiles in the explosion radius
+        grid[k][l] = "";
+      }
     }
-    //we get all of the tiles in the explosion radius
-    let tilesInExplosionRadius = [];
-    //first we need to check if the explosion radius is within the grid, if not we change it to the maximum possible value
-    if(i - explosionRadius < 0){
-        explosionRadius = i;
-    }
-    if(j - explosionRadius < 0){
-        explosionRadius = j;
-    }
-    if(i + explosionRadius > grid.length - 1){
-        explosionRadius = grid.length - 1 - i;
-    }
-    if(j + explosionRadius > grid[0].length - 1){
-        explosionRadius = grid[0].length - 1 - j;
-    }
-    //we get all of the tiles in the explosion radius, centered on the explosive tile. And we change them to ""
-    for(let k = i - explosionRadius; k <= i + explosionRadius; k++){
-        for(let l = j - explosionRadius; l <= j + explosionRadius; l++){
-            tilesInExplosionRadius.push([k, l]);
-            grid[k][l] = "";
-        }
-    }
-
-
+  }
 }
