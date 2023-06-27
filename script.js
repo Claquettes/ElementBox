554e3250c22c19dc2d0ce34d675eb6b3354179af
Claquettes <math@claq.fr>
2023-06-27 11:20:50 +0200

    particle generator in a different file

--- script.js
+++ script.js
@@ -287,30 +287,8 @@ function applyGravity() {
         }
         //if the tile is a generator
         if (grid[i][j] === "electrifiedGenerator") {
-          //we generate a tile below the generator/ the tile generated is of the type as one of the 4 tiles adjacent to the generator
-          //we check the 4 tiles adjacent to the generator
-          let adjacentTiles = [];
-          if (grid[i + 1][j] !== "" && grid[i + 1][j] !== "generator" && grid[i + 1][j] !== "wire" && grid[i + 1][j] !== "electrifiedGenerator" && grid[i + 1][j] !== "electrifiedWire") {
-            adjacentTiles.push(grid[i + 1][j]);
-          }
-          if (grid[i - 1][j] !== "" && grid[i - 1][j] !== "generator" && grid[i - 1][j] !== "wire" && grid[i - 1][j] !== "electrifiedGenerator" && grid[i - 1][j] !== "electrifiedWire") {
-            adjacentTiles.push(grid[i - 1][j]);
-          }
-          if (grid[i][j + 1] !== "" && grid[i][j + 1] !== "generator" && grid[i][j + 1] !== "wire" && grid[i][j + 1] !== "electrifiedGenerator" && grid[i][j + 1] !== "electrifiedWire") {
-            adjacentTiles.push(grid[i][j + 1]);
-          }
-          if (grid[i][j - 1] !== "" && grid[i][j - 1] !== "generator" && grid[i][j - 1] !== "wire" && grid[i][j - 1] !== "electrifiedGenerator" && grid[i][j - 1] !== "electrifiedWire") {
-            adjacentTiles.push(grid[i][j - 1]);
-          }
-          //we generate a tile below the generator
-          if (adjacentTiles.length > 0) {
-            if (grid[i + 1][j] === "") {
-              grid[i + 1][j] =
-                adjacentTiles[Math.floor(Math.random() * adjacentTiles.length)];
-            }
-          }
+          particleGenerator(i, j);
         }
-
         //if the tile is acid
         if (grid[i][j] === "acid") {
           acidInteraction(i, j);
