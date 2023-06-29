70603605e4f670796226cbf64e257877e3f5b50b
Claquettes <math@claq.fr>
2023-06-29 19:21:15 +0200

    electricity spreads only One block, but dissapear if we destroy the generator

--- src/interactions/electricity.js
+++ src/interactions/electricity.js
@@ -5,53 +5,81 @@ function calculateElectricity(i, j) {
   } else {
     let adjacentTilesForElectricity = [];
     //we call the adjacent4Tiles function to get the 4 adjacent tiles
-    adjacentTilesForElectricity = adjacent4Tiles(i, j, adjacentTilesForElectricity);
+    adjacentTilesForElectricity = adjacent4Tiles(
+      i,
+      j,
+      adjacentTilesForElectricity
+    );
+    let numberOfBatteries = 0;
+    let numberOfElectrifiedWires = 0;
     for (let k = 0; k < adjacentTilesForElectricity.length; k++) {
-      //we check if the tile is in the electrifiedTiles array
-      if (electrifiedTiles.includes(adjacentTilesForElectricity[k])) {
-        //we if the electrified tile is still electrified
-        hasElectricity = true;
-
+      if (adjacentTilesForElectricity[k] === "battery") {
+        numberOfBatteries++;
+      }
+      if (adjacentTilesForElectricity[k] === "electrifiedWire") {
+        numberOfElectrifiedWires++;
       }
     }
-    if(hasElectricity){
-    // Transform the tile accordingly to the electricity
-    switch (grid[i][j]) {
-      case "water":
-        if (hasElectricity) {
-          grid[i][j] = "electrifiedWater";
-        }
-        break;
-      case "wire":
-        if (hasElectricity) {
-          grid[i][j] = "electrifiedWire";
-        }
-        break;
-      case "generator":
-        if (hasElectricity) {
-          grid[i][j] = "electrifiedGenerator";
-        }
-        break;
-      case "electrifiedWater":
-        if (!hasElectricity) {
+    //we check if there is at least one battery or 2 electrifiedWires
+    if (numberOfBatteries > 0 || numberOfElectrifiedWires > 1) {
+      hasElectricity = true;
+    }
+  
+
+    if (hasElectricity) {
+      // Transform the tile accordingly to the electricity
+      switch (grid[i][j]) {
+        case "water":
+          if (hasElectricity) {
+            grid[i][j] = "electrifiedWater";
+          }
+          break;
+        case "wire":
+          if (hasElectricity) {
+            grid[i][j] = "electrifiedWire";
+          }
+          break;
+        case "generator":
+          if (hasElectricity) {
+            grid[i][j] = "electrifiedGenerator";
+          }
+          break;
+        case "electrifiedWater":
+          if (!hasElectricity) {
+            grid[i][j] = "water";
+          }
+          break;
+        case "electrifiedWire":
+          if (!hasElectricity) {
+            grid[i][j] = "wire";
+          }
+          break;
+        case "electrifiedGenerator":
+          if (!hasElectricity) {
+            grid[i][j] = "generator";
+          }
+          break;
+        case "led":
+          if (hasElectricity) {
+            grid[i][j] = "electrifiedLed";
+          }
+          break;
+      }
+    } else {
+      //we transform the electrified tiles into normal tiles
+      switch (grid[i][j]) {
+        case "electrifiedWater":
           grid[i][j] = "water";
-        }
-        break;
-      case "electrifiedWire":
-        if (!hasElectricity) {
+          break;
+        case "electrifiedWire":
           grid[i][j] = "wire";
-        }
-        break;
-      case "electrifiedGenerator":
-        if (!hasElectricity) {
+          break;
+        case "electrifiedGenerator":
           grid[i][j] = "generator";
-        }
-        break;
-      case "led":
-        if (hasElectricity) {
-          grid[i][j] = "electrifiedLed";
-        }
-        break;
+          break;
+        case "electrifiedLed":
+          grid[i][j] = "led";
+          break;
       }
     }
   }
